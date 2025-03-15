51f5e3f42be262c40198ea39a754a97e799f2aa2
Maged Eshak <magedeshak36@gmail.com>
2025-03-15 15:41:35 +0200

    WeekThree

---
+++ From_18_to_22/weekThree.js
@@ -0,0 +1,32 @@
+console.log("Assignment One:-");
+console.log("=".repeat(20));
+//--------------- 1 -------------------------
+
+
+//--------------- 2 -------------------------
+console.log("=".repeat(20));
+console.log("Assignment Two:-");
+console.log("=".repeat(20));
+
+
+//--------------- 3 -------------------------
+console.log("Assignment Three:-");
+console.log("=".repeat(20));
+
+
+//--------------- 4 -------------------------
+console.log("=".repeat(20));
+console.log("Assignment Four:-");
+console.log("=".repeat(20));
+
+
+//--------------- 5 -------------------------
+console.log("=".repeat(20));
+console.log("Assignment Five:-");
+console.log("=".repeat(20));
+
+
+//--------------- 6 -------------------------
+console.log("=".repeat(20));
+console.log("Assignment Six:-");
+console.log("=".repeat(20));
