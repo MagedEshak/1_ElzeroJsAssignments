4c6493be37144e2ca5356d000aa3c67148052ad1
Maged Eshak <magedeshak36@gmail.com>
2025-03-15 21:47:11 +0200

    ok

--- From_31_to_32/weekFive1.js
+++ From_31_to_32/weekFive1.js
@@ -2,51 +2,39 @@ console.log("Assignment One:-");
 console.log("=".repeat(20));
 //--------------- 1 -------------------------
 
-let userName = "Elzero";
-console.log(userName[0].toLowerCase()); // e
-console.log(userName.slice(0,1).toLowerCase()); // e
-console.log(userName.split("")[0].toLowerCase()); // e
-console.log(userName.substring(0,1).toLowerCase()); // e
-console.log(userName.match("E")[0].toLowerCase()); // e
-console.log(userName.charAt(0).toLowerCase().repeat(3)); // eee
+console.log(100 == "100"); // true
+console.log(100 < 1000); // true
+console.log(110 > 100 > 10 < 20); // true
+console.log(-10 == "-10"); // true
+console.log(! -50 == !+"-40"); // true
+console.log(! 10 <= -"-40"); // true
+console.log(!typeof "10" == !typeof 10); // true
+console.log(! 20 == false); // true
 
 //--------------- 2 -------------------------
 console.log("=".repeat(20));
 console.log("Assignment Two:-");
 console.log("=".repeat(20));
 
-let word = "Elzero";
-let letterZ = "z";
-let letterE = "e";
-let letterO = "O";
+let num1 = 10;
+let num2 = 20;
 
-console.log(word.includes(letterZ)); // True
-console.log(word.startsWith(letterE.toUpperCase())); // True
-console.log(word.endsWith(letterO.toLowerCase())); // True
+console.log(num1 < num2); // true
+console.log(num2 > num1); // true
+console.log(num1 != num2); // true
+console.log(typeof num1 === typeof num2); // true
+console.log(num1 <= num2); // true
+console.log(num2 >= num1); // true
 
-//---------------  String Challenge -------------------------
+//--------------- 3 -------------------------
 console.log("=".repeat(20));
-console.log(" String Challenge");
+console.log("Assignment Three:-");
 console.log("=".repeat(20));
-/*
-  String Challenge
-  All Solutions Must Be In One Chain
-  You Can Use Concatenate
-*/
 
-let a = "Elzero Web School";
+let a = 20;
+let b = 30;
+let c = 10;
 
-// Include This Method In Your Solution [slice, charAt]
-console.log(a.charAt(2).toUpperCase() + a.slice(3,6)); // Zero
-
-// 8 H
-console.log(a.charAt(13).toUpperCase().repeat(8)); // HHHHHHHH
-
-// Return Array
-console.log(a.split(" ",1)); // ["Elzero"]
-
-// Use Only "substr" Method + Template Literals In Your Solution
-console.log(`${a.substr(0,7)}${a.substr(11)}`); // Elzero School
-
-// Solution Must Be Dynamic Because String May Changes
-console.log(); // eLZERO WEB SCHOOl
+console.log(a < b && a > c || a < b); // true
+console.log(a < b && a > c); // true
+console.log(!(a > b) && !(a > b) && !(a < c) && !(a < c)); // true
